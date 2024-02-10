6e579dfd63ec95de1104aeeb0014d5410e4a5c03
Mainul Islam <mainulislam6168@gmail.com>
2024-02-10 22:33:39 +0600

    delete operation done. checked via postMan

--- index.js
+++ index.js
@@ -53,6 +53,14 @@ async function run() {
       res.send(result);
     })
 
+    // 4. Delete specific data
+    app.delete('/toys/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      result = await toyCollections.deleteOne(query);
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -61,7 +69,7 @@ async function run() {
     // Ensures that the client will close when you finish/error
   }
 }
-run().catch(console.log);
+run().catch(console.dir);
 
 
 app.listen(port, () => {
